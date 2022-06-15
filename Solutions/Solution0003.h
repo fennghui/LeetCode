ddd4d69c523dc74c80732e26e00db6b7ac5cd175
MatureAndCapable <2575750214@qq.com>
2022-06-15 16:50:06 +0800

    3. 无重复字符的最长子串 mid 经典双指针

---
+++ Solutions/Solution0003.h
@@ -0,0 +1,48 @@
+//
+// Created by hpf on 22-6-15.
+//
+
+#ifndef FH_A01_SOLUTION0003_H
+#define FH_A01_SOLUTION0003_H
+
+#include "../Common.h"
+//3. 无重复字符的最长子串 mid 经典双指针
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
+/**
+ * 经典双指针
+ */
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxSubLen = 0;
+        int prev = 0;
+        set<char> subChar;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (subChar.find(s[i]) == subChar.end()) {
+                //未在set中找到重复的
+                subChar.emplace(s[i]);
+            } else {
+                //找到重复的，将prev指针定位到重复的元素上
+                if ((i - prev) > maxSubLen) {
+                    maxSubLen = i - prev;
+                }
+                for (int j = prev; j < i; ++j) {
+                    if(s[j]==s[i]){
+                        prev = j+1;
+                        break;
+                    }else{
+                        subChar.erase(s[j]);
+                    };
+                }
+            }
+        }
+        if ((n - prev) > maxSubLen) {
+            maxSubLen = n - prev;
+        }
+        return maxSubLen;
+    }
+};
+
+#endif //FH_A01_SOLUTION0003_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "Solutions/Solution2104.h"
+#include "Solutions/Solution0003.h"
 int main() {
 //    vector<vector<int>> board{vector<int>{1,0}};
     Solution solution;
     vector<int> temp{1,3,3};
-    cout<<solution.subArrayRanges(temp)<<endl;
+    cout<<solution.lengthOfLongestSubstring(" ")<<endl;
     return 0;
 }
