e2a9aa9e639e20896b8335245905856c2a114139
fennghui <irisfeng@stu.pku.edu.cn>
2022-07-17 23:34:32 +0800

    104. 二叉树的最大深度 ez

---
+++ Solutions/Solution0104.h
@@ -0,0 +1,49 @@
+//
+// Created by irisfeng on 2022/7/17.
+//
+
+#ifndef FH_A01_SOLUTION0104_H
+#define FH_A01_SOLUTION0104_H
+
+#include "../Common.h"
+
+//104. 二叉树的最大深度 ez
+//给定一个二叉树，找出其最大深度。
+//
+//二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+//说明: 叶子节点是指没有子节点的节点。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int depth = 0;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            depth++; // 记录深度
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+        }
+        return depth;
+    }
+};
+
+#endif //FH_A01_SOLUTION0104_H
