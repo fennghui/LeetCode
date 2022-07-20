899690117ae3450ff463df43dc68811e92820cc5
fennghui <irisfeng@stu.pku.edu.cn>
2022-07-20 19:25:13 +0800

    654. 最大二叉树 mid

--- Solutions/Solution0654.h
+++ Solutions/Solution0654.h
@@ -7,7 +7,7 @@
 
 #include "../Common.h"
 
-//654. 最大二叉树
+//654. 最大二叉树 mid
 //给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
 //
 //创建一个根节点，其值为 nums 中的最大值。
@@ -17,8 +17,28 @@
 
 class Solution {
 public:
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+    TreeNode *constructMaximumBinaryTree(vector<int> &nums) {
+        int n = nums.size();
+        return traversal(nums, 0, n-1);
+    }
 
+    TreeNode *traversal(vector<int> &nums, int begin, int end) {
+        if (begin > end)
+            return nullptr;
+
+        int maxValue = nums[begin];
+        int maxIndex = begin;
+        for (int i = begin + 1; i <= end; ++i) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+                maxIndex = i;
+            }
+        }
+
+        TreeNode *node = new TreeNode(maxValue);
+        node->left = traversal(nums, begin, maxIndex - 1);
+        node->right = traversal(nums, maxIndex + 1, end);
+        return node;
     }
 };
 
