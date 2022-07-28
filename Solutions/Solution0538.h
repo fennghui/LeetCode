9a5231e07ff36a0d3d3f3c6ea77664fc57bb3c1e
fennghui <irisfeng@stu.pku.edu.cn>
2022-07-28 20:15:06 +0800

    538. 把二叉搜索树转换为累加树 mid

--- Solutions/Solution0538.h
+++ Solutions/Solution0538.h
@@ -7,7 +7,7 @@
 
 #include "../Common.h"
 
-//538. 把二叉搜索树转换为累加树
+//538. 把二叉搜索树转换为累加树 mid
 //给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），
 //使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
 //
@@ -19,8 +19,18 @@
 
 class Solution {
 public:
+    int sum = 0;
     TreeNode* convertBST(TreeNode* root) {
+        traversal(root);
+        return root;
+    }
 
+    void traversal(TreeNode* root){
+        if(root == nullptr) return;
+        traversal(root->right);
+        root->val += sum;
+        sum = root->val;
+        traversal(root->left);
     }
 };
 
