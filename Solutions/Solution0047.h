2706c6c9e68ecb9b15ea41cde354356f4981f929
fennghui <irisfeng@stu.pku.edu.cn>
2022-08-03 16:49:08 +0800

    47. 全排列 II mid

--- Solutions/Solution0047.h
+++ Solutions/Solution0047.h
@@ -7,13 +7,37 @@
 
 #include "../Common.h"
 
-//47. 全排列 II
+//47. 全排列 II mid
 //给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
 
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    vector<vector<int>> res;
+    vector<int> path;
 
+    vector<vector<int>> permuteUnique(vector<int> &nums) {
+        //sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+        backtracking(nums, used);
+        return res;
+    }
+
+    void backtracking(vector<int> &nums, vector<bool> &used) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        int isUsed[21] = {0};
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i] || isUsed[nums[i] + 10] == 1)
+                continue;
+            isUsed[nums[i] + 10] = 1;
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtracking(nums, used);
+            used[i] = false;
+            path.pop_back();
+        }
     }
 };
 
