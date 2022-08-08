0ff8c5f8414b391c614b307d6e474c8d8d65ebfc
fennghui <irisfeng@stu.pku.edu.cn>
2022-08-08 10:40:48 +0800

    435. 无重叠区间 mid

--- Solutions/Solution0435.h
+++ Solutions/Solution0435.h
@@ -7,9 +7,28 @@
 
 #include "../Common.h"
 
-//435. 无重叠区间
+//435. 无重叠区间 mid
 //给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重叠
 
+class Solution {
+public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
 
+    int eraseOverlapIntervals(vector<vector<int>> &intervals) {
+        if (intervals.size() == 0) return 0;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int res = 1;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if(intervals[i][0] < intervals[i-1][1]){
+                res++;
+            }else{
+               intervals[i][1] = min(intervals[i][1], intervals[i-1][1]);
+            }
+        }
+        return intervals.size() - res;
+    }
+};
 
 #endif //FH_A01_SOLUTION0435_H
---
+++ Solutions/Solution0763.h
@@ -0,0 +1,20 @@
+//
+// Created by irisfeng on 2022/8/8.
+//
+
+#ifndef FH_A01_SOLUTION0763_H
+#define FH_A01_SOLUTION0763_H
+
+#include "../Common.h"
+
+//763. 划分字母区间
+//字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+
+    }
+};
+
+#endif //FH_A01_SOLUTION0763_H
