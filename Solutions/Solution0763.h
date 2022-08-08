dd5bdd2e7df70c7e9277e2e60e5995cfdd342e97
fennghui <irisfeng@stu.pku.edu.cn>
2022-08-08 11:44:05 +0800

    763. 划分字母区间 mid

--- Solutions/Solution0763.h
+++ Solutions/Solution0763.h
@@ -7,13 +7,28 @@
 
 #include "../Common.h"
 
-//763. 划分字母区间
+//763. 划分字母区间 mid
 //字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
 
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+        unordered_map<char, int> umap;
+        for (int i = 0; i < s.size(); ++i) {
+            umap[s[i]] = i;
+        }
+        vector<int> res;
+        int preIndex = 0;
+        int curIndex = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            curIndex = max(curIndex, umap[s[i]]);
+            if (i == curIndex) {
+                res.push_back(curIndex - preIndex + 1);
+                preIndex = curIndex + 1;
+            }
+        }
 
+        return res;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Solutions/Solution0045.h"
+#include "Solutions/Solution0763.h"
 
 int main() {
 //    vector<vector<int>> board{vector<int>{1,0}};
@@ -7,14 +7,13 @@ int main() {
     vector<vector<char>> vec1{{'A', 'B', 'C', 'E'},
                               {'S', 'F', 'C', 'S'},
                               {'A', 'D', 'E', 'E'}};
-    vector<int> vec2{1,2,3};
-    auto result = solution.jump(vec2);
-    cout << result <<endl;
-//    for (int i = 0; i < result.size(); ++i){
-//        for(int j = 0; j < result[i].size(); ++j)
-//            cout << result[i][j] << ", ";
-//        cout<<endl;
-//    }
+    vector<int> vec2{1, 2, 3};
+    string s = "ababcbacadefegdehijhklij";
+    auto result = solution.partitionLabels(s);
+//    cout << result << endl;
+    for (int i = 0; i < result.size(); ++i){
+        cout<<result[i]<<endl;
+    }
 
 
 
